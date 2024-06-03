f034ce86d23de36725e1621deb2a03a9de5b1929
Jot43 <139986366+Jot43@users.noreply.github.com>
2024-06-03 13:28:15 +0530

    Update users_settings.py

--- bot/modules/users_settings.py
+++ bot/modules/users_settings.py
@@ -167,7 +167,7 @@ async def get_user_settings(from_user, key=None, edit_type=None, edit_mode=None)
         buttons.ibutton(f"{'✅️' if lsuffix != 'Not Exists' else ''} Leech Suffix", f"userset {user_id} lsuffix")
 
         buttons.ibutton("Metadata", f"userset {user_id} metadata")
-        lmetadata = user_dict.get('metadata', 'Not Exists')
+        metadata = user_dict.get('metadata', 'Not Exists')
                 
         lremname = 'Not Exists' if (val:=user_dict.get('lremname', config_dict.get('LEECH_FILENAME_REMNAME', ''))) == '' else val
         buttons.ibutton(f"{'✅️' if lremname != 'Not Exists' else ''} Leech Remname", f"userset {user_id} lremname")
